2b08487ac63c4387288f65017bb4c91787cab47f
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-06-08 18:17:19 -0400

    Update load_fastq.sh

--- load_fastq.sh
+++ load_fastq.sh
@@ -24,3 +24,31 @@ curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/025/SRR11799725/SRR11799725_1.
 curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/025/SRR11799725/SRR11799725_2.fastq.gz -o SRR11799725_GSM4556948_P14_OIR_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
 curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/023/SRR11799723/SRR11799723_1.fastq.gz -o SRR11799723_GSM4556946_P14_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
 curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/023/SRR11799723/SRR11799723_2.fastq.gz -o SRR11799723_GSM4556946_P14_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+
+cd /mnt/g/Sun_Ye_PR/GSE63472
+
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/034/SRR11799734/SRR11799734_1.fastq.gz -o SRR11799734_GSM4556957_P17_OIR_whole_retina_rep2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/034/SRR11799734/SRR11799734_2.fastq.gz -o SRR11799734_GSM4556957_P17_OIR_whole_retina_rep2_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/033/SRR11799733/SRR11799733_1.fastq.gz -o SRR11799733_GSM4556956_P17_OIR_whole_retina_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/033/SRR11799733/SRR11799733_2.fastq.gz -o SRR11799733_GSM4556956_P17_OIR_whole_retina_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/031/SRR11799731/SRR11799731_1.fastq.gz -o SRR11799731_GSM4556954_P17_NORM_whole_retina_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/031/SRR11799731/SRR11799731_2.fastq.gz -o SRR11799731_GSM4556954_P17_NORM_whole_retina_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/029/SRR11799729/SRR11799729_1.fastq.gz -o SRR11799729_GSM4556952_P14_OIR_whole_retina_rep3_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/029/SRR11799729/SRR11799729_2.fastq.gz -o SRR11799729_GSM4556952_P14_OIR_whole_retina_rep3_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/028/SRR11799728/SRR11799728_1.fastq.gz -o SRR11799728_GSM4556951_P14_OIR_whole_retina_rep2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/028/SRR11799728/SRR11799728_2.fastq.gz -o SRR11799728_GSM4556951_P14_OIR_whole_retina_rep2_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/030/SRR11799730/SRR11799730_1.fastq.gz -o SRR11799730_GSM4556953_P17_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/030/SRR11799730/SRR11799730_2.fastq.gz -o SRR11799730_GSM4556953_P17_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/032/SRR11799732/SRR11799732_1.fastq.gz -o SRR11799732_GSM4556955_P17_OIR_Rod_depleted_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/032/SRR11799732/SRR11799732_2.fastq.gz -o SRR11799732_GSM4556955_P17_OIR_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/027/SRR11799727/SRR11799727_1.fastq.gz -o SRR11799727_GSM4556950_P14_OIR_whole_retina_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/027/SRR11799727/SRR11799727_2.fastq.gz -o SRR11799727_GSM4556950_P14_OIR_whole_retina_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/026/SRR11799726/SRR11799726_1.fastq.gz -o SRR11799726_GSM4556949_P14_OIR_Rod_depleted_rep2_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/026/SRR11799726/SRR11799726_2.fastq.gz -o SRR11799726_GSM4556949_P14_OIR_Rod_depleted_rep2_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/024/SRR11799724/SRR11799724_1.fastq.gz -o SRR11799724_GSM4556947_P14_NORM_whole_retina_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/024/SRR11799724/SRR11799724_2.fastq.gz -o SRR11799724_GSM4556947_P14_NORM_whole_retina_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/025/SRR11799725/SRR11799725_1.fastq.gz -o SRR11799725_GSM4556948_P14_OIR_Rod_depleted_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/025/SRR11799725/SRR11799725_2.fastq.gz -o SRR11799725_GSM4556948_P14_OIR_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/023/SRR11799723/SRR11799723_1.fastq.gz -o SRR11799723_GSM4556946_P14_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR117/023/SRR11799723/SRR11799723_2.fastq.gz -o SRR11799723_GSM4556946_P14_NORM_Rod_depleted_rep1_Mus_musculus_RNA-Seq_2.fastq.gz
